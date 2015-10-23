1a2a577b468b0ee5b0cf63b8b8b8d55b3755820f
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-10-22 21:16:07 -0700

    pypy

---
+++ bootstrap_pypy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+brew install pypy;
+# compatibility of packages: https://bitbucket.org/pypy/compatibility/wiki/Home
+# pypy -m pip install git+https://bitbucket.org/pypy/numpy.git
