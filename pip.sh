1973fec0adda00686382098dab106bdc33e7b237
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2016-01-16 11:08:18 -0800

    percol

--- pip.sh
+++ pip.sh
@@ -4,6 +4,7 @@ sudo pip install cheat;
 sudo pip install tldr;
 sudo pip install tmuxp;
 sudo pip install thefuck;  # alias to "funk";
+sudo pip install percol;
 
 # Fix PIL, or comment out PIL inside requirements.txt, or use pillow instead
 # sudo pip install PIL --allow-external PIL --allow-unverified PIL;
