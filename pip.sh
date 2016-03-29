adbeebe104204d3cb32063be7f822d3d6411d0b0
Daniel D. Zhang <idf@users.noreply.github.com>
2016-03-29 00:29:25 -0700

    Update pip.sh

--- pip.sh
+++ pip.sh
@@ -1,10 +1,11 @@
-sudo -v;
-sudo pip install requests[security];  # SSL connection
-sudo pip install cheat;
-sudo pip install tldr;
-sudo pip install tmuxp;
-sudo pip install thefuck;  # alias to "funk";
-sudo pip install percol;
+sudo -v
+pip install -U setuptools
+sudo pip install requests[security]  # SSL connection
+sudo pip install cheat
+sudo pip install tldr
+sudo pip install tmuxp
+sudo pip install thefuck  # alias to "funk";
+sudo pip install percol
 
 # Fix PIL, or comment out PIL inside requirements.txt, or use pillow instead
 # sudo pip install PIL --allow-external PIL --allow-unverified PIL;
