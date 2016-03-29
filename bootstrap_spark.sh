5937806435ca4f64dde5f4db3422408fc5aa2182
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2016-03-29 07:59:04 +0000

    spark

---
+++ bootstrap_spark.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# credit: https://github.com/gzc
+sudo -v
+
+DEB_FILE=cdh5-repository_1.0_all.deb
+wget http://archive.cloudera.com/cdh5/one-click-install/trusty/amd64/$DEB_FILE -O /tmp/$DEB_FILE
+sudo dpkg -i /tmp/$DEB_FILE
+rm -f /tmp/$DEB_FILE
+sudo apt-get update
+sudo apt-get install -y default-jre
+sudo apt-get install -y default-jdk
+sudo apt-get install -y spark-master spark-worker spark-python
