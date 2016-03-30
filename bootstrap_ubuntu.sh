2a88a73504a9b37024c7b0295f280e66356a54dc
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2016-03-30 04:53:09 +0000

    texinfo

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-sudo -v 
+sudo -v
 
 apt-get update
 sudo apt-get install -y git
 sudo apt-get install -y python-pip
 sudo apt-get install -y emacs
 sudo apt-get install -y python-dev
-sudo pip install cheat
-sudo apt-get install git-svn
+sudo apt-get install -y git-svn
+sudo apt-get install -y texinfo
