9da5d99fa5f9dedcc2bd0a38af7786721f67ce35
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2016-03-30 17:36:14 +0000

    docker list

--- bootstrap_scipy.sh
+++ bootstrap_scipy.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 sudo -v
 sudo easy_install numpy
 sudo apt-get -y build-dep python-scipy
-sudo pip install scipy 
+sudo pip install scipy
 sudo pip install ipython[all]
+sudo apt-get install libfreetype6-dev libpng-dev
+sudo pip install matplotlib
--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -3,7 +3,7 @@ sudo -v
 
 apt-get update
 sudo apt-get install -y git
-sudo apt-get install -y python-pip
+sudo apt-get install -y python-pip  # may use bootstrap_pip.sh to get the latest
 sudo apt-get install -y emacs
 sudo apt-get install -y python-dev
 sudo apt-get install -y git-svn
