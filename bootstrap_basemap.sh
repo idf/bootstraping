f4e544c96f22b795943628511f8b99130f73f1d2
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2016-04-19 15:21:43 +0000

    basemap

---
+++ bootstrap_basemap.sh
@@ -0,0 +1,8 @@
+# Installation script of basemap for Ubuntu
+sudo -v
+sudo apt-get install python-tk
+sudo apt-get install python-matplotlib
+sudo apt-get install python-mpltoolkits.basemap
+
+# In Python
+# from mpl_toolkits.basemap import Basemap
