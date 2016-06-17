f7b1bbd155493c15ff055546a1a08a2282e499e9
Daniel D. Zhang <dzhang.idf@gmail.com>
2016-06-16 22:13:44 -0700

    Update bootstrap_ubuntu.sh

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -4,7 +4,7 @@ sudo -v
 apt-get update
 sudo apt-get install -y git
 sudo apt-get install -y python-pip  # may use bootstrap_pip.sh to get the latest
-sudo apt-get install -y emacs
+sudo apt-get install -y emacs --with-gnutls
 sudo apt-get install -y python-dev
 sudo apt-get install -y git-svn
 sudo apt-get install -y texinfo
