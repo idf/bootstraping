811133c8a29668680aad573b561a45a76e4a4ccb
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2016-07-14 22:59:35 -0700

    hh ubuntu

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 sudo -v
 
+sudo add-apt-repository ppa:ultradvorka/ppa  # for hh
 apt-get update
 sudo apt-get install -y git
 sudo apt-get install -y python-pip  # may use bootstrap_pip.sh to get the latest
@@ -9,3 +10,4 @@ sudo apt-get install -y python-dev
 sudo apt-get install -y git-svn
 sudo apt-get install -y texinfo
 sudo apt-get install -y tree
+sudo apt-get install -y hh
