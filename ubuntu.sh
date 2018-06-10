6ae1e492062ef530fd7138ab0991a145355494d7
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2018-06-10 05:59:59 +0000

    add python3 support

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,8 +5,10 @@ sudo add-apt-repository ppa:ultradvorka/ppa  # for hh
 apt-get update
 sudo apt-get install -y git
 sudo apt-get install -y python-pip  # may use bootstrap_pip.sh to get the latest
+sudo apt-get install -y python3-pip
 sudo apt-get install -y emacs --with-gnutls
 sudo apt-get install -y python-dev
+sudo apt-get install -y python3-dev
 sudo apt-get install -y git-svn
 sudo apt-get install -y texinfo
 sudo apt-get install -y tree
